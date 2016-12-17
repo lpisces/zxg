7893944f4dd62988dd2580a8f40f9014efc93695
liuxu <liuxu@gmail.com>
2016-12-17 17:08:41 +0800

    add finance(not finished)

--- core/fundamental.py
+++ core/fundamental.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup as bs
 import utils
 import pandas as pd
 import numpy as np
+import requests
 
 # 公司概况
 def brief(board, code):
@@ -143,3 +144,20 @@ def allotment(board, code):
     info.append(row)
   return pd.DataFrame(info)
 
+# 财务
+def financial_report(code, year, quarter):
+  url = "http://www.cninfo.com.cn/information/stock/financialreport_.jsp?stockCode=%s&key=%s" %(code, np.random.uniform(0, 1))
+  data = {}
+  q = {1: "-03-31", 2:"-06-30", 3:"-09-30", 4:"-12-31"}
+  data["yyyy"] = year
+  data["mm"] = q[quarter]
+  data["cwzb"] = "financialreport"
+  data["button2"] = u"提交".encode("gbk")
+  headers = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36", "referer": "http://www.cninfo.com.cn/information/stock/financialreport_.jsp?stockCode=000001&key=0.22836368174212307"}
+  r = requests.post(url, data=data, headers = headers)
+  r = requests.get(url, cookies=dict(JSESSIONID=r.cookies['JSESSIONID']), headers=headers)
+  soup = bs(r.text, "html5lib")
+  tr = soup.select(".zx_left table tr")
+  print r.text
+
+print financial_report("000002", "2016", 1)
