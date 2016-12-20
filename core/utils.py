357ed6cf82a8352aa93c5a61155a421575b0df8f
liuxu <liuxu@gmail.com>
2016-12-20 16:06:38 +0800

    fixed

--- core/utils.py
+++ core/utils.py
@@ -17,4 +17,18 @@ def get(url, headers = None, retry = 3, pause = 3):
     if retry > 0:
       return r
   return None
+
+def post(url, data, headers = None, retry = 3, pause = 3):
+  if headers == None:
+    headers = {"Accept-Encoding": "gzip, deflate, sdch", "user_agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"}
+  while retry > 0:
+    try:
+      r = requests.post(url, data = data, headers = headers)
+    except Exception as e:
+      retry -= 1
+      time.sleep(pause)
+      continue
+    if retry > 0:
+      return r
+  return None
  
